95f49f2c8d074461314f0eac8663add02cfd6f34
CelestialAnthem <python.spider.08@gmail.com>
2024-04-02 22:24:36 +0800

    cleanvision images done

--- data_juicer/ops/mycleanlab/cleanvision_mycleanlab.py
+++ data_juicer/ops/mycleanlab/cleanvision_mycleanlab.py
@@ -42,27 +42,43 @@ class CleanvisionMycleanlab(Mycleanlab):
     #         sample[DEFAULT_PREFIX + issue] = self.res_df.iloc[[index]].get(issue).to_list()[0]
     #     return sample
 
-    def save_results(self, sample):
-        print("hahaha")
-        print(sample)
+    def save_results(self, sample, sample_index):
         for issue in self.issues:
-            index = self.index_lookup.get(sample.get(self.image_key)[0])
-            if index is not None:
-                sample[DEFAULT_PREFIX + issue] = self.res_df.iloc[[index]].get(issue).to_list()[0]
+            images_list = sample.get(self.image_key)
+            for i in range(len(images_list)):
+                path_index = self.index_lookup.get(images_list[i])
+                if path_index is not None:
+                    for p_ind in path_index:
+                        p_index_split = [int(_) for _ in p_ind.split("-")]
+                        if p_index_split[0] == sample_index:
+                            if sample.get(DEFAULT_PREFIX + issue, None) is None:
+                                sample[DEFAULT_PREFIX + issue] = [False for _ in range(len(images_list))]
+                            sample[DEFAULT_PREFIX + issue][p_index_split[1]] = self.res_df.iloc[[p_index_split[2]]].get(issue).to_list()[0]
+
         return sample
     
+    
+    def create_index_list(self, list_of_lists):
+        index_list = []
+        for i, sublist in enumerate(list_of_lists):
+            for j, _ in enumerate(sublist):
+                count = len(index_list)
+                index_list.append(f"{i}-{j}-{count}")
+        return index_list
+
+
     def process(self, dataset, num_proc):
         image_paths = dataset[self.image_key]
         hf_dataset_lst, res_df_lst = [], []
         chunk_size = 100000
         for j, image_pathxs in enumerate([image_paths[i : i + chunk_size] for i in range(0, len(image_paths), chunk_size)]):
             def worker(_):
-                return Image.open(_[0])
+                return [Image.open(x) for x in _] 
             with ThreadPool(processes = num_proc) as pool:
                 image_keys = list(tqdm(pool.imap(worker, image_pathxs), total=len(image_pathxs), desc='Images Loading'))
                 pool.terminate()
-                
-            my_dict = {self.image_key: image_keys, self.image_key + "_path": dataset[self.image_key][j * chunk_size : (j + 1) * chunk_size]}
+            
+            my_dict = {self.image_key: sum(image_keys, []), self.image_key + "_path": sum(dataset[self.image_key][j * chunk_size : (j + 1) * chunk_size], [])}
             tmp_dataset = Dataset.from_dict(my_dict)
             imagelab = Imagelab(hf_dataset=tmp_dataset, image_key=self.image_key)
             imagelab.find_issues()
@@ -72,9 +88,22 @@ class CleanvisionMycleanlab(Mycleanlab):
         self.hf_dataset = concatenate_datasets(hf_dataset_lst)
         # print(self.hf_dataset['images_path'])
         self.res_df = pd.concat(res_df_lst)
-        
-        self.index_lookup = {v[0]: i for i, v in enumerate(self.hf_dataset[self.image_key + "_path"])}
-        print(self.index_lookup)
-        dataset = dataset.map(self.save_results)        
+        print(self.res_df)
+        img_list = self.hf_dataset[self.image_key + "_path"]
+        print("img_list", img_list)
+        index_list = self.create_index_list(image_paths)
+        print("index_list", index_list)
+        self.index_lookup = {}
+        for img, index in zip(img_list, index_list):
+            if img in self.index_lookup:
+                self.index_lookup[img].append(index)
+            else:
+                self.index_lookup[img] = [index]
+        print("lookup", self.index_lookup)
+        # print(self.index_lookup)
+        # dataset = dataset.map(lambda pair: self.save_results(pair[1], pair[0]), enumerate(dataset))   
+        # dataset = dataset.map(lambda idx, sample: self.save_results(sample, idx), enumerate(dataset))
+        dataset = dataset.map(lambda item, index: self.save_results(item, index), with_indices=True) 
         return dataset
+            
             
